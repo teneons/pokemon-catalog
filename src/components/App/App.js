a46c01efad519f15f3b62a96e11c164e591ac60e
teneons <teneons@gmail.com>
2020-10-08 13:56:01 +0300

    made base markup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 
-function App(props) {
+function App() {
   return (
-    <div className="App">
-      42
-      {props.pdata}
+    <div className="App bg-dark container-fluid col-12 m-0 p-0" style={{height: '100vh'}}>
+      <Jumbotron fluid className="h-100 w-100 d-inline-block bg-white" >
+        <Container className='col-12 d-flex justify-content-center text-warning shadow-sm'><h1>Pokemon Catalog</h1></Container>
+        <Container>42</Container>
+      </Jumbotron>
     </div>
   );
 }
