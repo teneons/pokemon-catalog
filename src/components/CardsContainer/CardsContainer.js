d2763fc93a8a69b54c51dcc15b8f36ed0852b1c5
teneons <teneons@gmail.com>
2020-10-11 20:42:31 +0300

    set and out data (test)

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,32 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import PokimonCard from '../PokemonCard/PokimonCard';
 
 const CardsContainer = () => {
 
-  const [pokiItems, setPokiData] = useState(null);
+  const [pokiItems, setPokiData] = useState();
   const [pokiErr, setError] = useState(false);
   const [pokiLoad, setLoad] = useState(true);
 
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')    //get data
-    .then(d=>d.json())
-    .then(pokiData => {   //set data
-      setPokiData(pokiData.results);
-      setLoad(false);})
-    .catch((err) => {     //error
-      setError(true)
-      alert(err)
-    })
-  }, [])    //nothing
+      .then(d => d.json())
+      .then(pokiData => {   //set data
+        setPokiData(pokiData.results[1]);
+        setLoad(false);
+      })
+      .catch((err) => {     //error
+        setError(true)
+        alert(err)
+      })
+  }, [])    //nothing 
 
-  return(
+  // const outData = pokiItems.map((item, id)=> {
+  //   return <Col><PokimonCard pokiItems={pokiItems}/></Col>
+  // })
+
+  return (
     <Container className='d-flex justify-content-around align-items-center shadow-sm bg-secondary'>
       <Row>
-        <Col><PokimonCard /></Col>
-        <Col><PokimonCard /></Col>
-        <Col><PokimonCard /></Col>
+        <Col><PokimonCard pokiItems={pokiItems} /></Col>
       </Row>
     </Container>
   )
--- src/components/PokemonCard/PokimonCard.js
+++ src/components/PokemonCard/PokimonCard.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import {Card, Button} from 'react-bootstrap';
 
-const PokimonCard = () => {
+const PokimonCard = (props) => {
 
   return (
     <Card style={{ width: '18rem' }} className='d-flex justify-content-center'>
       <Card.Img variant="top" src="https://pokeres.bastionbot.org/images/pokemon/1.png" />
       <Card.Body>
-        <Card.Title>Pickchu</Card.Title>
-        <Button variant="danger" className='mr-1'>Fire</Button>
-        <Button variant="danger">Fire</Button>
+    <Card.Title>{props.pokiItems.name}</Card.Title>
+        <Button variant="danger" className='mr-1'>More info</Button>
       </Card.Body>
     </Card>
   )
