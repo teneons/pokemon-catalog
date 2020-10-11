b88265c93f04e9cbe6aa4ca7838c9469d5e97e88
teneons <teneons@gmail.com>
2020-10-11 21:50:44 +0300

    fix problem with data

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import PokimonCard from '../PokemonCard/PokimonCard';
 
 const CardsContainer = () => {
 
-  const [pokiItems, setPokiData] = useState();
+  const [pokiItems, setPokiData] = useState(42);
   const [pokiErr, setError] = useState(false);
   const [pokiLoad, setLoad] = useState(true);
 
@@ -13,14 +13,13 @@ const CardsContainer = () => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')    //get data
       .then(d => d.json())
       .then(pokiData => {   //set data
-        setPokiData(pokiData.results[1]);
+        setPokiData(pokiData.results[2]);
         setLoad(false);
       })
       .catch((err) => {     //error
         setError(true)
-        alert(err)
       })
-  }, [])    //nothing 
+  }, [pokiItems])    //nothing 
 
   // const outData = pokiItems.map((item, id)=> {
   //   return <Col><PokimonCard pokiItems={pokiItems}/></Col>
@@ -29,7 +28,7 @@ const CardsContainer = () => {
   return (
     <Container className='d-flex justify-content-around align-items-center shadow-sm bg-secondary'>
       <Row>
-        <Col><PokimonCard pokiItems={pokiItems} /></Col>
+      <Col><PokimonCard pokiItems={pokiItems}/></Col>
       </Row>
     </Container>
   )
