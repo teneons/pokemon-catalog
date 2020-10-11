5e262ed6d5104d36e244ec4693c9de81426113ea
teneons <teneons@gmail.com>
2020-10-11 18:28:18 +0300

    added processing datas

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,17 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import PokimonCard from '../PokemonCard/PokimonCard';
 
 const CardsContainer = () => {
 
+  const [pokiItems, setPokiData] = useState(null);
+  const [pokiErr, setError] = useState(false);
+  const [pokiLoad, setLoad] = useState(true);
+
+
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')    //get data
     .then(d=>d.json())
-    .then(pokiData => console.log(pokiData.results[42].name))
-    .catch((err) => {
+    .then(pokiData => {   //set data
+      setPokiData(pokiData.results);
+      setLoad(false);})
+    .catch((err) => {     //error
+      setError(true)
       alert(err)
     })
-  }, [])
+  }, [])    //nothing
 
   return(
     <Container className='d-flex justify-content-around align-items-center shadow-sm bg-secondary'>
