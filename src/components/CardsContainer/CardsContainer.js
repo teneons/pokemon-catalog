943da47fecec88f9eb84f14b896e10a2e0352aaf
teneons <teneons@gmail.com>
2020-10-11 17:23:19 +0300

    made (hook) api request

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import PokimonCard from '../PokemonCard/PokimonCard';
 
 const CardsContainer = () => {
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(d=>d.json())
+    .then(pokiData => console.log(pokiData.results[42].name))
+    .catch((err) => {
+      alert(err)
+    })
+  }, [])
 
   return(
     <Container className='d-flex justify-content-around align-items-center shadow-sm bg-secondary'>
