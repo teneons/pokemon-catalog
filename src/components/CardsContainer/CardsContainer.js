5d308ca5228fcc122cf79cbad6ecad68f558393c
teneons <teneons@gmail.com>
2021-02-01 00:51:01 +0200

    corrcted UI for main cards block

--- src/components/CardsContainer/CardsContainer.js
+++ src/components/CardsContainer/CardsContainer.js
@@ -21,24 +21,28 @@ const CardsContainer = () => {
       })
   }, [])    //nothing 
 
-  const pokiDataBox = [pokiItems[0], pokiItems[1], pokiItems[2]]
+  const pokiDataBox = [pokiItems[0], pokiItems[1], pokiItems[2]];
 
+  const loadMore = (e) => {
+    e.preventDefault();
+    return <OutCards pokiDataBox={pokiDataBox} />
+  }
 
   //render components
   if (pokiLoad === true) {
     return (
-      <Container className='d-flex flex-column justify-content-around align-items-center bg-secondary '>
-        <div class="spinner-border text-light" style={{ width: '10rem', height: '10rem' }} role="status">
-          <span class="sr-only">Loading data..</span>
+      <Container className='d-flex flex-column justify-content-around align-items-center col-12' style={{height: 'calc(100vh - 8.4vh)'}}>
+        <div className="spinner-border" variant="dark" style={{ width: '10rem', height: '10rem' }} role="status">
+          <span className="sr-only">Loading data..</span>
         </div>
       </Container>
     )
   } else if (pokiLoad === false) {
     return (
-      <div className='d-flex flex-column align-items-center'>
+      <Container className='d-flex flex-column align-items-center'>
         <OutCards pokiDataBox={pokiDataBox} />
-        <Button variant="btn btn-info" className='m-2'>Load more..</Button>
-      </div>
+        <Button variant="btn btn-info" className='m-2' onClick={()=> <OutCards pokiDataBox={pokiDataBox} />}>Load more..</Button>
+      </Container>
     )
   } else if (pokiErr === true) {
     return (
