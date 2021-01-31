6db22885ed700d353fbaf5c7eff643b86c8c703f
teneons <teneons@gmail.com>
2021-02-01 00:15:37 +0200

    corrected UI for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,10 @@ import logo from './pokemonLogo.svg';
 const Header = () => {
 
   return (
-    <Navbar bg="dark" variant="dark" className='d-flex justify-content-center'>
+    <Navbar bg="dark" variant="dark" expand="lg" className='d-flex justify-content-center' >
       <Navbar.Brand className='text-uppercase font-weight-bold'>
       <img alt="Pokemon Catalog" src={logo} width="30" height="30" className="d-inline-block align-top"/>
-          {' '}
-          Pokemon Catalog
+        Pokemon Catalog
     </Navbar.Brand>
     </Navbar>
   )
