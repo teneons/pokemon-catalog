ee03e3d6de56a6c4d97f27fd55738e3f9e6ce512
teneons <teneons@gmail.com>
2021-02-01 00:58:40 +0200

    corrected UI for pokemon card;renamed file comp.

--- src/components/OutCards/OutCards.js
+++ src/components/OutCards/OutCards.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Container, Row, Col} from 'react-bootstrap';
-import PokimonCard from '../PokemonCard/PokimonCard';
+import PokemonCard from '../PokemonCard/PokemonCard';
 
 const OutCards = (props) => {
    return(
     <Container className='d-flex justify-content-around align-items-center row row-col-3 col-12'>
-      <PokimonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
-      <PokimonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
-      <PokimonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
-      <PokimonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
-      <PokimonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
+      <PokemonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
+      <PokemonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
+      <PokemonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
+      <PokemonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
+      <PokemonCard pokiItems={props.pokiDataBox[0]} pokiId={1} />
     </Container>
    )
 }
--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -5,10 +5,10 @@ const PokimonCard = (props) => {
   const imgUrl = `https://pokeres.bastionbot.org/images/pokemon/${props.pokiId}.png`;
 
   return (
-    <Card style={{ width: '18rem' }} className='d-flex justify-content-center m-3'>
-      <Card.Img variant="top" src={imgUrl} />
+    <Card className='d-flex justify-content-center m-3'>
+      <Card.Img variant="top" src={imgUrl} style={{ width: '16rem' }} />
       <Card.Body className='d-flex flex-column align-items-center'>
-        <Card.Title style={{fontSize: '2.2rem'}}>{props.pokiItems.name}</Card.Title>
+        <Card.Title>{props.pokiItems.name}</Card.Title>
         <Button variant="outline-danger" className='mr-1'>Info</Button>
       </Card.Body>
     </Card>
