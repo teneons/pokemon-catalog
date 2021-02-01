511948a53a954869ca7d68b30590d48b66024958
teneons <teneons@gmail.com>
2021-02-01 18:44:26 +0200

    corrected fetch request

--- src/components/PokemonModal/PokemonModal.js
+++ src/components/PokemonModal/PokemonModal.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import {Button, Modal} from 'react-bootstrap';
+import {Button, Modal, Table} from 'react-bootstrap';
 
 const PokemonModal = (props) => {
+  
   //will mount
   useEffect(() => {
-    console.log(props.pokemonUrl)
     handleShow()
+    pokemonModalRequest()
   }, [props.showModal])
 
 
@@ -14,34 +15,27 @@ const PokemonModal = (props) => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(props.showModal);
 
-    
+
   //request
-  const pokeminModalRequest = () => {
+  const pokemonModalRequest = () => {
     fetch(props.pokemonUrl)    //get data
-    .then(d => d.json())
-    .then(pokeData => {   //set data
-      setPokeData(pokeData.results);
-      setLoad(false);
-    })
-    .catch(() => {     //error
-      setError(true)
-    })
+    .then(dJson => dJson.json())
+    .then(data => setPokemonData(data))  //set data
   }
+
+  const [pokemonData, setPokemonData] = useState(42)
+
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{pokemonData.name}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+          <Button variant="secondary" onClick={handleClose}>Ok</Button>
         </Modal.Footer>
       </Modal>
     </>
